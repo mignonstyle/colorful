55bb4ebd0c28ae69aefd296fe5825f80cd874e00
mignonstyle <mignonxstyle@gmail.com>
2016-12-06 23:13:45 +0900

    modify custom menu

--- inc/custom-menu.php
+++ inc/custom-menu.php
@@ -6,12 +6,12 @@
  * @author Mignon Style
  * @license GPLv2 or later
  */
-if ( ! function_exists( 'colorful_setup_custom_menu' ) ) :
-function colorful_setup_custom_menu() {
+if ( ! function_exists( 'colorful_custom_menu_setup' ) ) :
+function colorful_custom_menu_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'colorful' ),
 	) );
 }
 endif;
-add_action( 'after_setup_theme', 'colorful_setup_custom_menu' );
+add_action( 'after_setup_theme', 'colorful_custom_menu_setup' );
