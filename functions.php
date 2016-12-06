a85c5dd9246016e29fd537601695fbec7ec0addb
mignonstyle <mignonxstyle@gmail.com>
2016-12-06 22:52:51 +0900

    add thumbnails file

--- functions.php
+++ functions.php
@@ -36,13 +36,6 @@ function colorful_setup() {
 	add_theme_support( 'title-tag' );
 
 	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
-
-	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
 	 */
---
+++ inc/thumbnails.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Thumbnails settings.
+ *
+ * @package colorful
+ * @author Mignon Style
+ * @license GPLv2 or later
+ */
+
+/**
+ * Thumbnails settings.
+ */
+if ( ! function_exists( 'colorful_setup_thumbnails' ) ) :
+function colorful_setup_thumbnails() {
+	// Enable support for Post Thumbnails on posts and pages.
+	add_theme_support( 'post-thumbnails' );
+
+	// Set the default Featured Image (formerly Post Thumbnail) dimensions.
+	set_post_thumbnail_size( 300, 300, true );
+
+	// Register a new image size.
+	add_image_size( 'large-thumbnail', 1000, 400, true );
+	add_image_size( 'middle-thumbnail', 1000, 400, true );
+	add_image_size( 'small-thumbnail', 500, 200, true );
+}
+endif;
+add_action( 'after_setup_theme', 'colorful_setup_thumbnails' );
