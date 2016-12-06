fc230a435b7a0c3b8fdfccd324f3f9ab40fa0140
mignonstyle <mignonxstyle@gmail.com>
2016-12-06 21:29:26 +0900

    add librarys

--- functions.php
+++ functions.php
@@ -98,21 +98,8 @@ function colorful_widgets_init() {
 }
 add_action( 'widgets_init', 'colorful_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
-function colorful_scripts() {
-	wp_enqueue_style( 'colorful-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'colorful-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'colorful-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'colorful_scripts' );
+// Enqueue scripts and styles.
+require get_template_directory() . '/inc/enqueue-scripts.php';
 
 /**
  * Implement the Custom Header feature.
