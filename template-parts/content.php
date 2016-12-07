5c717a55e2f69a13a5936c435f42a64d01bfc450
mignonstyle <mignonxstyle@gmail.com>
2016-12-08 02:06:11 +0900

    add entry header function

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,16 +12,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-		?>
-
-		<?php
-		// Prints posting meta information in content header.
-		colorful_entry_meta_header();
+		// Prints the entry header.
+		colorful_entry_header();
 		?>
 	</header><!-- .entry-header -->
 
