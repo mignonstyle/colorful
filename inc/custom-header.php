8b387c0af508fab89e4e3f9eae09c49c5c82e6a8
mignonstyle <mignonxstyle@gmail.com>
2016-12-15 18:18:16 +0900

    Format the code

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -5,16 +5,6 @@
  * @package colorful
  * @author  Mignon Style
  * @license GPLv2 or later
- *
-	<?php if ( get_header_image() ) : ?>
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-	</a>
-	<?php endif; // End header image check. ?>
- *
- * @link https://developer.wordpress.org/themes/functionality/custom-headers/
- *
- * @package colorful
  */
 
 /**
